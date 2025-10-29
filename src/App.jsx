2f93e5861bd6076516d1ab7bd93c39c3939b664e
Sam Cabrera <71114474+samanthacabrera@users.noreply.github.com>
2025-10-29 12:58:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
 function App() {
-
   return (
-    <>
-      <header className="flex items-center justify-between w-full h-20 px-8 bg-[#ffedd3] uppercase tracking-widest font-sans text-sm md:text-base">
-        <div className="flex items-center space-x-8">
-          <h1 className="text-2xl font-bold tracking-wide">COACHELLA</h1>
-          <h3 className="font-light">APR 10-12 & 17-19, 2026</h3>
-        </div>
-
-        <nav className="hidden md:flex space-x-6 font-medium">
-          <a href="#">Music</a>
-          <a href="#">Passes</a>
-          <a href="#">Plan</a>
-          <a href="#">Experience</a>
-          <a href="#">Shop</a>
-          <a href="#" className="font-bold">Join Waitlist</a>
-        </nav>
-
-        <div className="hidden md:block">
-          ⌕
-        </div>
-      </header>
-
-      <main></main>
-      <footer></footer>
-    </>
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-4xl">vite-react-tailwind-template</h1>
+    </div>
   )
 }
 
